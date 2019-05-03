fea22776f15ef09e31a6d7332d854a31b56140e6
Gabriel <gabicardetti@gmail.com>
2019-05-03 17:55:08 -0300

    now I can choose which playlist to download

--- run.py
+++ run.py
@@ -5,9 +5,16 @@ user = input("Enter the username: ")
 dirname = input("Enter dirname: ")
 spotify = spot.Spot(user)
 data = spotify.get_playlist_user()
-playlist = data[0]['uri']
-print(playlist)
-tracks = spotify.get_tracks_playlist(playlist)
+
+i = 0
+for item in data:
+    print(i, item["playlist_name"])
+    i = i+1
+
+playlist = int(input("Which playlist do you want to download? "))
+playlist = data[playlist]
+print("Dowland ", playlist["playlist_name"])
+tracks = spotify.get_tracks_playlist(playlist["uri"])
 tracks = spotify.list_for_search(tracks)
 
 you = youtu.Youtu(dirname)
